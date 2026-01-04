9bcc577ccca38acb7a2213e16f79637241ca9410
Wasifur Rahman Efaz <110248681+efaaz@users.noreply.github.com>
2026-01-04 18:03:08 +0600

    Add implementations for LCS and Fibonacci algorithms

---
+++ algorithm/DP/LCS.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int lcs(string x, string y, int n, int m) {
+    if(n == 0 || m == 0)
+        return 0;
+ 
+    if(x[n-1] == y[m-1])
+        return 1 + lcs(x, y, n-1, m-1);
+ 
+    else
+        return max(lcs(x, y, n-1, m), lcs(x, y, n, m-1));
+}
+ 
+int main() {
+    string x = "abcfghn";
+    string y= "abedfhn";
+    
+ 
+    int n = x.length();
+    int m = y.length();
+ 
+    cout << "LCS Length = " << lcs(x, y, n, m) << endl;
+ 
+    return 0;
+}
---
+++ algorithm/DP/fibonnaci.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n) {
+    int fib[n + 1];
+    fib[0] = 0;
+    fib[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+        fib[i] = fib[i - 1] + fib[i - 2];
+
+    return fib[n];
+}
+
+int main() {
+    int n = 10;
+    cout << "Fibonacci(" << n << ") = " << fibonacci(n);
+}
